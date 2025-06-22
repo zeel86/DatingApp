a8a5f04c39ee62525c06da77c1a186aa5507207f
zeel86 <shyambambal18@gmail.com>
2025-06-22 16:41:02 +0530

    Client created

--- API/Program.cs
+++ API/Program.cs
@@ -4,27 +4,38 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-//register controllers
 builder.Services.AddControllers();
 
-//Connection string
+// Register CORS policy BEFORE building the app
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyMethod()
+              .AllowAnyHeader()
+              .WithOrigins("http://localhost:4200"); // Use http if Angular is not using https
+    });
+});
+
+// Configure database
 builder.Services.AddDbContext<DataContext>(options =>
 {
-   // This automatically loads appsettings.Development.json when in Development environment
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Use CORS middleware
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.MapControllers();
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5001/api/users').subscribe(data => {
+      this.users = data;
+    },
+      error => {
+        throw error("Failed!");
+      }
+    );
+  }
+}
